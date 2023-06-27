324696f07021b4e51d84da2a6464071d8d6323ac
TaoMaru <jackson_maria1@columbusstate.edu>
2023-06-27 16:57:51 -0400

    Remove unused imports

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 using ClassPractice.Math;
 
@@ -18,7 +15,7 @@ namespace ClassPractice
     {
         static void Main(string[] args)
         {
-            /*var maru = new Person();
+            var maru = new Person();
             maru.setName("Tao", "Maru");
 
             maru.Introduce();
@@ -26,7 +23,7 @@ namespace ClassPractice
             Calculator calcutron = new Calculator();
             var answer = calcutron.Add(99, 1);
             Console.WriteLine(answer);
-            */
+            
 
             var numbers = new int[3] { 10, 20, 30};
             for (int index = 0; index < numbers.Length; index++) {
