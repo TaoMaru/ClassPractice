fc582d1320a175bc921acb14735cd1c04866110d
TaoMaru <jackson_maria1@columbusstate.edu>
2023-06-27 16:50:30 -0400

    Add team status enum and work with direct enum or enum "id" as int

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ using ClassPractice.Math;
 
 namespace ClassPractice
 {
+    public enum TeamStatus { 
+        Registered,
+        CheckedIn,
+        Disqualified,
+        Active
+    }
     class Program
     {
         static void Main(string[] args)
@@ -26,6 +32,15 @@ namespace ClassPractice
             for (int index = 0; index < numbers.Length; index++) {
                 Console.WriteLine(numbers[index]);
             }
+
+            string theNums = string.Join("->", numbers);
+            Console.WriteLine(theNums);
+
+            var teamAStatus = TeamStatus.CheckedIn;
+            Console.WriteLine(teamAStatus); // team status as "checkedIn"
+            Console.WriteLine((int)teamAStatus); //team status as ID "1"
+            var statusID = 3; // given a status ID...
+            Console.WriteLine((TeamStatus)statusID); //team status as "Active"
         }
     }
 }
